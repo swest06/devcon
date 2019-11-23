5c00a0a1731d0b7a9d3c2a91f29c27a6ffb907cf
unknown <swest06@dcs.bbk.ac.uk>
2019-11-23 16:36:03 +0000

    update

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Placeholder Name</h1>
+          <p className="lead">
+            Are you a creative? Whether you're a Designer or a Developer, create
+            a profile and collaborate with other creatives.
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
