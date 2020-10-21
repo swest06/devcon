3b900be11490ceca3773c45366d33eb739397c56
unknown <swest06@dcs.bbk.ac.uk>
2020-10-21 11:30:59 +0100

    update

---
+++ client/build/precache-manifest.7e1b0e78646dca6f72e0e6797f87c947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ed24edcd7516fa109f39bb64a3de05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a589f5492b3d8519bd4",
+    "url": "/static/css/main.36135690.chunk.css"
+  },
+  {
+    "revision": "f1621f723b9c8182625c",
+    "url": "/static/js/2.6a758ef7.chunk.js"
+  },
+  {
+    "revision": "3a589f5492b3d8519bd4",
+    "url": "/static/js/main.7f7877a6.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "071081e4ca39be0206cdb552e5a0f527",
+    "url": "/static/media/network-2402637_1280.071081e4.jpg"
+  }
+]);
